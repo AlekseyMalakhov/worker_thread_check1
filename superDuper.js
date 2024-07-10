0cd2a8eccf1a474503b0b013d3d6e680b11f05dc
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-07-10 16:59:32 +0300

    remove answers

--- superDuper.js
+++ superDuper.js
@@ -24,19 +24,14 @@ for (let i = 0; i < numberOfThreadsWeUse; i++) {
         taskValue[1] = finish;
         start = finish + 1;
         finish = start + portionPerThread - 1;
-        // console.log("i = " + i);
-        // console.log("taskValue:", taskValue);
-        // console.log("__________");
     } else {
         taskValue[0] = start;
         taskValue[1] = initialValue;
-        // console.log("i = " + i + " last");
-        // console.log("taskValue:", taskValue);
     }
     valuesForTasks.push(taskValue);
 }
 
-console.log("valuesForTasks:", valuesForTasks);
+//console.log("valuesForTasks:", valuesForTasks);
 
 const heavyTask = (start, finish) => {
     let res = 0;
@@ -55,9 +50,10 @@ for (let i = 0; i < valuesForTasks.length; i++) {
 }
 //console.log("tasks:", tasks);
 
+console.log("Turbo calculating...");
 Promise.all(tasks)
     .then((result) => {
-        console.log(result);
+        //console.log(result);
         const sum = result.reduce((a, b) => a + b);
         console.log(sum);
     })
@@ -70,16 +66,3 @@ Promise.all(tasks)
 
 // const res1 = heavyTask(0, 100);
 // console.log("res1:", res1);
-
-/*
-pool.exec(heavyTask, [100000, 100000])
-    .then(function (result) {
-        console.log("result1 = ", result);
-    })
-    .catch(function (err) {
-        console.error(err);
-    })
-    .then(function () {
-        pool.terminate(); // terminate all workers when done
-    });
-*/
