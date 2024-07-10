552677af0970b6bec365246852d7431da3b32b13
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-07-10 15:15:04 +0300

    correct answer

--- answer.js
+++ answer.js
@@ -1,7 +1,3 @@
-import workerpool from "workerpool";
-
-const pool = workerpool.pool();
-
 function loop1() {
     let res = 0;
     for (var i = 10000000000; i < 20000000000; i++) {
@@ -17,8 +13,12 @@ function loop2() {
     return res;
 }
 
-const res1 = loop1();
-console.log("res1:", res1);
+//this code will run on a single core
 
-const res2 = loop2();
-console.log("res2:", res2);
+//first loop1
+const result1 = loop1();
+console.log("result1:", result1); //result1: 149999999990002750000
+
+//and then loop2
+const result2 = loop2();
+console.log("result2:", result2); //result2: 49999999990067860000
